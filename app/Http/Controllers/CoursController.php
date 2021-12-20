5caa9946ec21b517ff1cec8d064a1a8c7f26926e
Matt MARTIN <codingstuff91@gmail.com>
2021-12-20 11:49:32 +0100

    Creation model Cours et composant associés

---
+++ app/Http/Controllers/CoursController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cours;
+use App\Http\Requests\StoreCoursRequest;
+use App\Http\Requests\UpdateCoursRequest;
+
+class CoursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCoursRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCoursRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cours  $cours
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cours $cours)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cours  $cours
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cours $cours)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCoursRequest  $request
+     * @param  \App\Models\Cours  $cours
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCoursRequest $request, Cours $cours)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cours  $cours
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cours $cours)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_12_20_104158_create_cours_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cours', function (Blueprint $table) {
+            $table->id();
+            $table->integer('eleve_id');
+            $table->integer('facture_id');
+            $table->timestamp('date_debut');
+            $table->timestamp('date_fin');
+            $table->text('notions_apprises');
+            $table->boolean('paye');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cours');
+    }
+}
