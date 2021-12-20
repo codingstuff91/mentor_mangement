997fb20a10535f51469c87a9aa0299af6e4d9eae
Matt MARTIN <codingstuff91@gmail.com>
2021-12-20 21:15:24 +0100

    Ajout bouton creation et modif style

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -5,9 +5,14 @@
         </h2>
     </x-slot>
 
-    <div class="py-6">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200 flex flex-row justify-between">
+                    <button class="p-2 bg-green-300 rounded-lg">
+                        <a href="{{ route('client.create') }}">Ajouter un client</a>
+                    </button>
+                </div>
                 <div class="p-4 bg-white border-b border-gray-200">
                     @foreach ($clients as $client)
                         <div class="flex justify-between my-4 w-1/2 mx-auto">
