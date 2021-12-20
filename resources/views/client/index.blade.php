bafb75b322753698b8b88bb414cba6d4e845ca1d
Matt MARTIN <codingstuff91@gmail.com>
2021-12-20 12:21:19 +0100

    Ajout lien creation client

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200 flex flex-row justify-between">
                     <h1>Liste des clients</h1>
+                    <button class="p-2 bg-green-300 rounded-lg">
+                        <a href="{{ route('client.create') }}">Ajouter un client</a>
+                    </button>
                 </div>
             </div>
         </div>
