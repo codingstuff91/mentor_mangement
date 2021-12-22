a901b98d92f0f29f44e25c04cf12d6301ecd99c8
Matt MARTIN <codingstuff91@gmail.com>
2021-12-22 16:23:05 +0100

    Affichage des cours

--- resources/views/cours/index.blade.php
+++ resources/views/cours/index.blade.php
@@ -5,23 +5,30 @@
         </h2>
     </x-slot>
 
-    <div class="py-4">
+    <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 flex flex-row justify-between">
+                <div class="p-2 bg-white border-b border-gray-200">
                     <button class="p-2 bg-green-300 rounded-lg">
                         <a href="{{ route('cours.create') }}">Ajouter un cours</a>
                     </button>
                 </div>
             </div>
-            <div class="my-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @foreach ($cours as $lecon)
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <h1>{{ $lecon->date_debut }} | {{ $lecon->nombre_heures }} heures</h1>
-                        <p>{!! $lecon->notions_apprises !!}</p>
+
+            @foreach ($cours as $lecon)
+            <div class="mt-2 w-1/2">
+                <div class="p-2 bg-white border-b border-gray-200 overflow-hidden shadow-sm sm:rounded-lg flex justify-between">
+                    <div class="flex flex-col">
+                        <h1 class="text-lg font-extrabold">{{ $lecon->date_formated }} | {{ $lecon->heure_debut }} --> {{ $lecon->heure_fin }} | {{ $lecon->nombre_heures }} heure{{ $lecon->nombre_heures > 1 ? "s" : "" }}</h1>
+                        <p class="mt-2">{!! $lecon->notions_apprises !!}</p>
                     </div>
-                @endforeach
+                    <div class="flex flex-row h-1/2">
+                        <button class="text-xs p-2 rounded-lg bg-blue-300"><a href="{{ route('cours.edit', $lecon->id) }}">Editer</a></button>
+                        <button class="text-xs p-2 rounded-lg bg-red-300 ml-2">Supprimer</button>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
 </x-app-layout>
