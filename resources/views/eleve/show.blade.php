62f10c9bd26cef1f27a94e78d404ff9943faf8e1
Matt MARTIN <codingstuff91@gmail.com>
2021-12-22 20:41:55 +0100

    Affichage des heures de cours et objectifs

--- resources/views/eleve/show.blade.php
+++ resources/views/eleve/show.blade.php
@@ -6,9 +6,19 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="mt-2 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 columns-2">
+            <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <h2 class="text-3xl text-center">Cours réalisés</h2>
+                @foreach ($eleve->cours as $cours)
+                    <div class="my-2 flex flex-row justify-between mx-auto w-2/3">
+                        <p>{{ $cours->date_formated }} | {{ $cours->heure_debut }} --> {{ $cours->heure_fin }}</p>
+                        <p>{{ $cours->nombre_heures }} Heure{{ $cours->nombre_heures > 1 ? "s" : ""}}</p>
+                    </div>
+                @endforeach
+            </div>
+            <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h2 class="text-3xl text-center">Objectifs</h2>
+                <p class="my-2 text-center">{!! $eleve->objectifs !!}</p>
             </div>
         </div>
     </div>
