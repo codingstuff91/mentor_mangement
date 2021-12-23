ed3a45566e8fd3cbef752c96b949be116cd42168
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2021-12-23 10:20:42 +0100

    Ajout de seeders (#8)
    * Seeder pour table users
    
    * maj userseeder
    
    * Ajout matiere seeder
    
    * Ajout seeder pour client

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\ClientSeeder;
+use Database\Seeders\MatiereSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            MatiereSeeder::class,
+            ClientSeeder::class
+        ]);
     }
 }
