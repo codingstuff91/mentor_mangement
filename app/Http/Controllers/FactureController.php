84347a23785fcfa6b4e4398173799b99c689246b
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2021-12-24 16:49:50 +0100

    Mise a jour du suivi des factures (#11)
    * Relation entre facture et cours
    
    * Affichage details de facture
    
    * Affichage heure liees a une facture
    
    * Ajout icones
    
    * Ajout total compte des heures dans facture
    
    * Ajout total facture
    
    * Ajout icone dans liste factures
    
    * Modif style titre avec ajout icone
    
    * Modif styles affichages des cours

--- app/Http/Controllers/FactureController.php
+++ app/Http/Controllers/FactureController.php
@@ -57,7 +57,21 @@ class FactureController extends Controller
      */
     public function show(Facture $facture)
     {
-        //
+        $cours = $facture->cours()->get();
+        $total_heures = 0;
+        $total_facture = 0;
+
+        foreach ($cours as $lecon) {
+            $total_heures += $lecon->nombre_heures;
+            $total_facture += $lecon->total_prix;
+        }
+
+        return view('facture.show')->with([
+            'facture' => $facture,
+            'cours' => $cours,
+            'total_heures' => $total_heures,
+            'total_facture' => $total_facture
+        ]);
     }
 
     /**
---
+++ resources/views/facture/show.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="p-2 rounded-lg bg-lime-200 font-semibold text-3xl text-gray-800 leading-tight">
+            <i class="fas fa-user mr-2"></i>{{ $facture->client->nom }} <i class="fas fa-calendar-day mx-2"></i> {{ $facture->month_year_creation }}
+        </h2>
+        <h2 class="p-2 rounded-lg bg-cyan-200 text-3xl text-center">{{ $total_facture }} €</h2>
+    </x-slot>
+
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
+            <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-gray-300 w-1/3 bg-blue-100">
+                <h2 class="text-3xl text-center">Nombre heures : {{ $total_heures }}</h2>
+            </div>
+            <div class="ml-4 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-gray-300 w-2/3 bg-blue-100">
+                <h2 class="p-2 text-3xl text-center">Cours concernés</h2>
+                @foreach ($cours as $lecon)
+                <div class="my-2 p-2 bg-white shadow-sm sm:rounded-lg border-2 border-gray-300 flex justify-between">
+                    <p><i class="fas fa-user mr-2"></i>{{ $lecon->eleve->nom }}</p>
+                    <p><i class="fas fa-calendar-day mr-2"></i>le {{ $lecon->date_formated }}</p>
+                    <p><i class="fas fa-clock mr-2"></i>{{ $lecon->heure_debut }} --> {{ $lecon->heure_fin }}</p>
+                    <p>{{ $lecon->nombre_heures }} heure{{ $lecon->nombre_heures > 1 ? "s" : "" }}</p>
+                    <p><i class="fas fa-euro-sign mr-2"></i>{{ $lecon->total_prix }}€</p>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
