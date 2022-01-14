f927a538bcda4964d31979ee2764ea419169da15
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-14 22:05:47 +0100

    Maj design page création de cours Rassemblement des champs date, heure debut et fin

--- resources/views/cours/create.blade.php
+++ resources/views/cours/create.blade.php
@@ -12,21 +12,29 @@
                     <form class="flex flex-col" action="{{ route('cours.store') }}" method="post">
                             @csrf
                             <label class="my-2">Eleve</label>
-                            <select name="eleve_id">
+                            <select class="rounded-lg" name="eleve_id">
                                 @foreach ($eleves as $eleve)
                                     <option value="{{ $eleve->id }}">{{ $eleve->nom }}</option>
                                 @endforeach
                             </select>
-
-                            <label class="my-2">Date début</label>
-                            <input type="date" name="date_debut" class="rounded-lg">
-
-                            <label class="my-2">Heure début</label>
-                            <input type="time" name="heure_debut" class="rounded-lg">
-
-                            <label class="my-2">Heure fin</label>
-                            <input type="time" name="heure_fin" class="rounded-lg">
                             
+                            <div class="my-2 flex flex-row justify-between">
+                                <div class="flex flex-col align-items-center">
+                                    <label class="my-2">Date du cours</label>
+                                    <input type="date" name="date_debut" class="rounded-lg">
+                                </div>
+
+                                <div class="flex flex-col align-items-center">
+                                    <label class="my-2">Heure début</label>
+                                    <input type="time" name="heure_debut" class="rounded-lg">
+                                </div>
+
+                                <div class="flex flex-col align-items-center">
+                                    <label class="my-2">Heure fin</label>
+                                    <input type="time" name="heure_fin" class="rounded-lg">                             
+                                </div>
+                            </div>
+
                             <label class="my-2">Notions apprises</label>
                             <textarea name="notions" class="rounded-lg" cols="30" rows="2"></textarea>
 
@@ -34,7 +42,7 @@
                             <input type="number" name="taux_horaire" class="rounded-lg">
 
                             <label class="my-2">Facture concernée</label>
-                            <select name="facture_id">
+                            <select class="rounded-lg" name="facture_id">
                                 @foreach ($factures as $facture)
                                     <option value="{{ $facture->id }}">{{ $facture->month_year_creation }} -- {{ $facture->client->nom }}</option>
                                 @endforeach
