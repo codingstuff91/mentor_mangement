68ba3bfc52a96704d0892d1cf6281e1d35442421
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-14 21:41:27 +0100

    Modification styles et ajout statut actif, inactif

--- resources/views/eleve/index.blade.php
+++ resources/views/eleve/index.blade.php
@@ -14,12 +14,18 @@
                     </button>
                 </div>
             </div>
-            <div class="mt-4 w-2/3 mx-auto p-2 bg-white border-b border-gray-200">
+            <div class="mt-4 w-full mx-auto p-2 bg-white border-b border-gray-200">
                 @foreach ($eleves as $eleve)
                     <div class="my-4 flex flex-row justify-between w-3/4 mx-auto">
                         <h2 class="text-3xl flex items-center">{{ $eleve->nom }}
+                            @if ($eleve->active)
+                                <span class="ml-2 p-2 text-xs rounded-lg bg-green-200"><i class="fas fa-check mr-2"></i>Actif</span>
+                            @else
+                                <span class="ml-2 p-2 text-xs rounded-lg bg-red-100"><i class="fas fa-ban mr-2"></i>Inactif</span>
+                            @endif
+                            
                             <span class="ml-2 p-2 text-xs rounded-lg bg-blue-100"><i class="fas fa-user mr-2"></i>{{ $eleve->client->nom }}</span>
-                            <span class="ml-2 p-2 text-xs rounded-lg bg-lime-200"><i class="fas fa-book mr-2"></i>{{ $eleve->matiere->nom }}</span>
+                            <span class="ml-2 p-2 text-xs rounded-lg bg-amber-300"><i class="fas fa-book mr-2"></i>{{ $eleve->matiere->nom }}</span>
                         </h2>
                         <button class="p-2 rounded-lg bg-blue-300"><a href="{{ route('eleve.show', $eleve->id) }}"><i class="fas fa-search mr-2"></i>Détails</a></button>
                     </div>
