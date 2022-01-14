0808a2d89177f66d428552e54e3b0eb5b1467293
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-14 22:06:42 +0100

    Modif titre principal

--- resources/views/cours/create.blade.php
+++ resources/views/cours/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Ajout de nouveau cours') }}
+            {{ __('Ajouter un cours') }}
         </h2>
     </x-slot>
 
