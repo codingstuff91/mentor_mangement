ff75c284337d3dfe5f963bd8a0385a3a5ed09602
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-17 21:10:18 +0100

    Ajout logique prise en compte de la checkbox pour les packs

--- app/Http/Controllers/CoursController.php
+++ app/Http/Controllers/CoursController.php
@@ -47,11 +47,14 @@ class CoursController extends Controller
     {
         $count_hours = $cours_service->count_lesson_hours($request->heure_fin,$request->heure_debut);
         
+        $pack_heures = isset($request->pack_heures) ? 1 : 0;
+        
         Cours::create([
             'eleve_id' => $request->eleve_id,
             'date_debut' => $request->date_debut ." ". $request->heure_debut,
             'date_fin' => $request->date_debut ." ". $request->heure_fin,
             'nombre_heures' => $count_hours,
+            'pack_heures' =>$pack_heures,
             'notions_apprises' => $request->notions,
             'paye' => false,
             'facture_id' => $request->facture_id,
