44b1beba4609b29fa3b7815706757fba65a5ad1e
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-21 18:49:25 +0100

    Application middleware auth globalement

--- routes/web.php
+++ routes/web.php
@@ -7,33 +7,26 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\FactureController;
 use App\Http\Controllers\MatiereController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+
+    Route::resource('eleve', EleveController::class);
+    
+    Route::resource('client', ClientController::class);
+    
+    Route::resource('matiere', MatiereController::class);
+    
+    Route::resource('cours', CoursController::class);
+    
+    Route::resource('facture', FactureController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
-Route::resource('eleve', EleveController::class);
-
-Route::resource('client', ClientController::class);
-
-Route::resource('matiere', MatiereController::class);
-
-Route::resource('cours', CoursController::class);
-
-Route::resource('facture', FactureController::class);
-
-require __DIR__.'/auth.php';
