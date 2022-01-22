ed7e35ab320edc87960af63d10cef6b0cfc23a64
Matt MARTIN <codingstuff91@gmail.com>
2022-01-22 21:28:30 +0100

    Refact seeder principal. Ajout factories imbriquees a plsrs niveaux

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cours;
+use App\Models\Eleve;
+use App\Models\Client;
+use App\Models\Facture;
+use App\Models\Matiere;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\ClientSeeder;
@@ -16,10 +21,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $clients = Client::factory(3)->create();
+
+        $factures = Facture::factory(3)->create([
+            'client_id' => $clients->random()->id
+        ]);
+
+        Matiere::factory(5)->create()->each(function($matiere) use ($clients){
+            Eleve::factory()->count(2)->create([
+                'client_id' => $clients->random()->id,
+                'matiere_id' => $matiere->id
+            ])->each(function($eleve){
+                Cours::factory(2)->create([
+                    'eleve_id' => $eleve->id,
+                    'facture_id' => Facture::all()->random()->id
+                ]);
+            });
+        });
+
         $this->call([
             UserSeeder::class,
-            MatiereSeeder::class,
-            ClientSeeder::class
         ]);
     }
 }
