3f837fe4822fda371ac9dfe860ef53ae7e52f257
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-25 21:44:00 +0100

    Maj validation edition d'un eleve

--- app/Http/Requests/UpdateEleveRequest.php
+++ app/Http/Requests/UpdateEleveRequest.php
@@ -13,7 +13,7 @@ class UpdateEleveRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class UpdateEleveRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nom' => 'required',
+            'active' => 'required',
+            'client_id' => 'required',
+            'matiere_id' => 'required'
         ];
     }
 }
