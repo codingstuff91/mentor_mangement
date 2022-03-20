df3b0f5b615392a90d2fe9e890bacc6e4a66a8c5
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2022-03-20 20:26:58 +0100

    Ajout reportings dans page dashboard (#27)
    * Simplification route dashboard
    
    * Creation nouveau controller pour le dashboard et recuperation des sommes de cours et des gains
    
    * Mise a jour affichage vue dashboard et ajout icones
    
    * Ajout nombre heures par matieres et maj style affichages
    
    * Maj affichage des donnees

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cours;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // Total d'heures de cours
+        $nb_heures_cours = Cours::select(DB::raw("SUM(nombre_heures) as total"))->get();
+
+        // Total argent gagné
+        $total_gains = Cours::select(DB::raw('SUM(nombre_heures * taux_horaire) as total'))->get();
+
+        // Affichage des heures de cours par matières
+        $heure_cours_par_matiere = DB::table('eleves')
+                                    ->join('cours', 'cours.eleve_id', '=', 'eleves.id')
+                                    ->join('matieres', 'matieres.id', '=', 'eleves.matiere_id')
+                                    ->select(DB::raw('matieres.nom, SUM(nombre_heures) as total'))
+                                    ->groupBy('matieres.nom')
+                                    ->orderBy('total', 'desc')
+                                    ->get();
+
+        return view('dashboard')->with([
+            'total_heures' => $nb_heures_cours,
+            'total_gains' => $total_gains,
+            'nombre_heures_par_eleve' => $heure_cours_par_matiere
+        ]);
+    }
+}
