6c8815aa1a965df05583fc8fb73b9c481594b924
Matt MARTIN <codingstuff91@gmail.com>
2022-03-20 19:28:46 +0100

    Ajout nombre heures par matieres et maj style affichages

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,9 +16,19 @@ class DashboardController extends Controller
         // Total argent gagné
         $total_gains = Cours::select(DB::raw('SUM(nombre_heures * taux_horaire) as total'))->get();
 
+        // Affichage des heures de cours par matières
+        $heure_cours_par_matiere = DB::table('eleves')
+                                    ->join('cours', 'cours.eleve_id', '=', 'eleves.id')
+                                    ->join('matieres', 'matieres.id', '=', 'eleves.matiere_id')
+                                    ->select(DB::raw('matieres.nom, SUM(nombre_heures) as total'))
+                                    ->groupBy('matieres.nom')
+                                    ->orderBy('total', 'desc')
+                                    ->get();
+
         return view('dashboard')->with([
             'total_heures' => $nb_heures_cours,
-            'total_gains' => $total_gains
+            'total_gains' => $total_gains,
+            'nombre_heures_par_eleve' => $heure_cours_par_matiere
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/3">
+            <div class="bg-white overflow-hidden shadow-sm rounded-xl sm:w-1/2 lg:w-1/3">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/teaching.png') }}">
@@ -16,7 +16,7 @@
                     <h3 class="mt-4 text-3xl text-center">{{ $total_heures[0]->total }}</h3>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/3 ml-4">
+            <div class="bg-white overflow-hidden shadow-sm rounded-xl sm:w-1/2 lg:w-1/3 ml-4">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/money.png') }}">
@@ -26,5 +26,30 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="mt-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
+            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-full">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h2 class="text-3xl text-bold text-center">Heures de cours par matières</h2>
+                    <table class="border-collapse mx-auto table-auto lg:w-1/3 sm:w-full mt-4">
+                        <thead>
+                            <tr>
+                                <th class="bg-blue-400 p-2 text-center">MATIERE</th>
+                                <th class="bg-blue-400 p-2 text-center">NOMBRE HEURES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @for ($i = 0; $i < $nombre_heures_par_eleve->count(); $i++)
+                                <tr>
+                                    <td class="p-2 bg-blue-200 text-center">{{ $nombre_heures_par_eleve[$i]->nom }}</td>
+                                    <td class="p-2 bg-blue-200 text-center">{{ $nombre_heures_par_eleve[$i]->total }} heures</td>
+                                </tr>
+                            @endfor
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>  
     </div>
 </x-app-layout>
