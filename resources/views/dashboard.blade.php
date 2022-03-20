7514cbaf9e5c07f6df518aa851d7573d4a324d01
Matt MARTIN <codingstuff91@gmail.com>
2022-03-20 20:23:17 +0100

    Maj affichage des donnees

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="my-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl sm:w-1/2 lg:w-1/3">
+            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/2">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/teaching.png') }}">
@@ -16,11 +16,11 @@
                     <h3 class="mt-4 text-3xl text-center">{{ $total_heures[0]->total }}</h3>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl sm:w-1/2 lg:w-1/3 ml-4">
+            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/2 ml-4">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/money.png') }}">
-                        <h2 class="ml-4 text-xl font-bold">Heures de cours données</h2>
+                        <h2 class="ml-4 text-xl font-bold">Total des revenus</h2>
                     </div>
                     <h3 class="mt-4 text-3xl text-center">{{ $total_gains[0]->total }}€</h3>
                 </div>
