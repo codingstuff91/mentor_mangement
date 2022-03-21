11beb9a1c68403fe8e1e11ca25df8d8220f38467
Matthieu MARTIN <codingstuff91@gmail.com>
2022-03-21 15:38:41 +0100

    Amelioration affichage dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="my-6">
+    <div class="my-6 pt-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/2">
+            <div class="bg-white overflow-hidden shadow-lg rounded-xl w-1/2">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/teaching.png') }}">
@@ -16,7 +16,7 @@
                     <h3 class="mt-4 text-3xl text-center">{{ $total_heures[0]->total }}</h3>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-1/2 ml-4">
+            <div class="bg-white overflow-hidden shadow-lg rounded-xl w-1/2 ml-4">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-row justify-center items-center">
                         <img src="{{ asset('img/money.png') }}">
@@ -29,7 +29,7 @@
     </div>
     <div class="mt-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row">
-            <div class="bg-white overflow-hidden shadow-sm rounded-xl w-full">
+            <div class="bg-white overflow-hidden shadow-lg rounded-xl w-full">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h2 class="text-3xl text-bold text-center">Heures de cours par matières</h2>
                     <table class="border-collapse mx-auto table-auto lg:w-1/3 sm:w-full mt-4">
