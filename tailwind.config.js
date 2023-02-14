bff401bdd47cf9e7d40962833fb9892621b1600d
Matthieu MARTIN <codingstuff91@gmail.com>
2023-02-14 22:25:21 +0100

    Custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
     },
 
     plugins: [require('@tailwindcss/forms')],
