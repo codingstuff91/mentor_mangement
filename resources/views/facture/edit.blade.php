867949cc8ce562584ba8a4f1d8b847c666a9a5c9
Matthieu MARTIN <codingstuff91@gmail.com>
2023-02-22 10:40:53 +0100

    Edited edit facture view

--- resources/views/facture/edit.blade.php
+++ resources/views/facture/edit.blade.php
@@ -1,20 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Editer un cours') }}
+            {{ __('Mise à jour statut facture') }}
         </h2>
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="w-1/3 mx-auto p-4 bg-white border-b border-gray-200">
+                <div class=" mx-auto p-4 bg-white border-b border-gray-200">
                     <form action="{{ route('facture.update', $facture->id) }}" method="post" class="flex flex-col">
                         @csrf
                         @method('patch')
 
                         <label class="mt-2">Facture payée</label>
-                        <select class="mt-2" name="payee" class="rounded-lg">
+                        <select class="mt-2 rounded-lg" name="payee" class="rounded-lg">
                             <option value="0" @if ($facture->payee == 0) selected="selected" @endif>NON</option>
                             <option value="1" @if ($facture->payee == 1) selected="selected" @endif>OUI</option>
                         </select>
