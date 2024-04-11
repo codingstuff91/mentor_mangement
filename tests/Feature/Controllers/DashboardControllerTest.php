c590ec3077583fc6c217df1249e98a3eb013acb0
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-04-11 15:26:19 +0200

    Edit test

--- tests/Feature/Controllers/DashboardControllerTest.php
+++ tests/Feature/Controllers/DashboardControllerTest.php
@@ -16,38 +16,33 @@ beforeEach(function () {
         ->for($this->customer);
 });
 
-test('can render the dashboard main page', function () {
-    $response = get(route('dashboard'));
-
-    expect($response->status())->toBe(200);
+test('render the dashboard main page', function () {
+    get(route('dashboard'))->assertOk();
 });
 
-test('can show the total of course hours given', function () {
+test('show the total of course hours given', function () {
     $response = get(route('dashboard'));
 
     $response->assertSeeText("Total Heures");
     $response->assertSee(Course::count());
 });
 
-test('can show the total of students', function () {
-    $response = get(route('dashboard'));
-
-    $response->assertSeeText("Total Eleves");
-    $response->assertSee(Student::count());
+test('show the total of students', function () {
+    get(route('dashboard'))
+        ->assertSeeText("Total Eleves")
+        ->assertSee(Student::count());
 });
 
-test('can show the total revenue', function () {
+test('show the total revenue', function () {
     $totalRevenues = Course::select(DB::raw('SUM(hours_count * hourly_rate) as total'))->first();
 
-    $response = get(route('dashboard'));
-    $response->assertSeeText("Total revenus");
-
-    $response->assertSee($totalRevenues['total']);
+    get(route('dashboard'))
+        ->assertSeeText("Total revenus")
+        ->assertSee($totalRevenues['total']);
 });
 
-test('can show the total of courses', function () {
-    $response = get(route('dashboard'));
-
-    $response->assertSeeText("Total Cours");
-    $response->assertSee(Course::count());
+test('show the total of courses', function () {
+    get(route('dashboard'))
+        ->assertSeeText("Total Cours")
+        ->assertSee(Course::count());
 });
