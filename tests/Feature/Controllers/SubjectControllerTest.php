ffcd920538eb5451d73f05f6f7b0e02992e9bfb1
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-04-11 15:41:58 +0200

    Refacto SubjectControllerTest

--- tests/Feature/Controllers/SubjectControllerTest.php
+++ tests/Feature/Controllers/SubjectControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Subject;
+use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
 use function Pest\Laravel\patch;
@@ -13,47 +14,42 @@ beforeEach(function () {
 });
 
 test('can render the subjects index view', function () {
-    $response = get(route('subject.index'));
-
-    expect($response->status())->toBe(200);
+    get(route('subject.index'))
+        ->assertOk();
 });
 
 test('can display all subjects on index view', function () {
-    $response = get(route('subject.index'));
-    $response->assertSee($this->subject->name);
+    get(route('subject.index'))
+        ->assertSee($this->subject->name);
 });
 
 test('can render the subject create view', function () {
-    $response = get(route('subject.create'));
-
-    $response->assertOk();
-    $response->assertSee('Nom de la matière');
+    get(route('subject.create'))
+        ->assertOk()
+        ->assertSee('Nom de la matière');
 });
 
-test('can store a new subject', function () {
+test('store a new subject', function () {
     post(route('subject.store'), [
         'name' => 'php',
     ]);
 
-    $this->assertDatabaseCount('subjects', 2);
+    assertDatabaseCount('subjects', 2);
 });
 
 test('cannot store a new subject without a name', function () {
-    $response = post(route('subject.store'), [
+    post(route('subject.store'), [
         'name' => '',
-    ]);
-
-    $response->assertSessionHasErrors(['name']);
+    ])->assertSessionHasErrors(['name']);
 });
 
-test('can render the edit view with subject informations', function () {
-    $response = get(route('subject.edit', $this->subject));
-
-    $response->assertOk();
-    $response->assertSee($this->subject->nom);
+test('render the edit view with subject informations', function () {
+    get(route('subject.edit', $this->subject))
+        ->assertOk()
+        ->assertSee($this->subject->nom);
 });
 
-test('can update a subject', function () {
+test('update a subject', function () {
     patch(route('subject.update', $this->subject),[
         'name' => 'excel'
     ]);
