52b5661e638faddcc46cc0debdc2c3e602b68a9e
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-05-02 08:59:54 +0200

    Remove old function

--- app/Services/CoursService.php
+++ app/Services/CoursService.php
@@ -2,27 +2,10 @@
 
 namespace App\Services;
 
-use App\Exceptions\InvalidCourseHoursOrderException;
 use Carbon\Carbon;
 
 class CoursService
 {
-    /**
-     * @param string $rawEndHour
-     * @param string $rawStartHour
-     * @return int
-     * @throws \Throwable
-     */
-    public function count_lesson_hours(string $rawEndHour, string $rawStartHour): int
-    {
-        $startHour = $this->getHour($rawStartHour);
-        $endHour = $this->getHour($rawEndHour);
-
-        throw_if($endHour < $startHour || $startHour > $endHour, InvalidCourseHoursOrderException::class);
-
-        return $endHour - $startHour;
-    }
-
     public static function computeEndHour(string $startHour, string $duration)
     {
         $hoursToAdd = CoursService::splitHoursOrMinutes($duration, 0);
